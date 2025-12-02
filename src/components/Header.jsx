968e9ce02b24aeb60f24dac218c4fee5447c36d6
Viviane <vivianeaguiarc@outlook.com>
2025-12-02 16:30:56 -0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,4 @@
+function Header(props){
+    return <header>{props.children}</header>
+}
+export default Header;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Header from "./Header";
+
+function Tasks() {
+    const [inputValue, setInputValue] = useState("teste");
+    const [messages, setMessages] = useState([
+        "Hello, how can I help you?",
+        "What is your name?",
+    ]);
+    function handleButtonClick(){
+        setMessages([...messages, inputValue]);
+    }
+    return (
+        <div>
+            <Header>
+                <h1>Add Task</h1>
+            </Header>
+
+            <input 
+                type="text" 
+                value={inputValue} 
+                placeholder="Create you task..."
+                onChange={(e) => setInputValue(e.target.value)} 
+            />
+            <button onClick={handleButtonClick}>Add Task</button>
+
+            <ul>
+                {messages.map((message, index) => (
+                    <li key={index}>{message}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+export default Tasks;
