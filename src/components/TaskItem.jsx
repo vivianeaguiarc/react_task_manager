25f7a717de0afdb6d4b65bd6e335561392075390
Viviane <vivianeaguiarc@outlook.com>
2025-12-04 15:21:28 -0300

    feat: ensure tasks are listed on Tasks components

---
+++ src/components/TaskItem.jsx
@@ -0,0 +1,27 @@
+const TaskItem = ({ task }) => {
+  const getStatusClasses = () => {
+    if (task.status === "done") {
+      return "bg-[#00ADB5]/10 text-[#00ADB5]"
+    }
+
+    if (task.status === "in_progress") {
+      return "bg-[#FFAA04]/10 text-[#FFAA04]"
+    }
+
+    if (task.status === "not_started") {
+      return "bg-[#35383E]/10 text-[#35383E]"
+    }
+
+    return ""
+  }
+
+  return (
+    <div
+      className={`flex items-center gap-2 rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`}
+    >
+      {task.title}
+    </div>
+  )
+}
+
+export default TaskItem
