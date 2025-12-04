477fdc426af3170e9f041a8b76648d9654ffeea4
Viviane <vivianeaguiarc@outlook.com>
2025-12-04 14:34:48 -0300

    chore: add images of aplication

---
+++ src/assets/icons/index.js
@@ -0,0 +1,31 @@
+import AddIcon from "./add.svg?react"
+import ArrowLeftIcon from "./arrow-left.svg?react"
+import CheckIcon from "./check.svg?react"
+import ChevronRightIcon from "./chevron-right.svg?react"
+import CloudSunIcon from "./cloud-sun.svg?react"
+import DetailsIcon from "./details.svg?react"
+import GlassWaterIcon from "./glass-water.svg?react"
+import HomeIcon from "./home.svg?react"
+import LoaderIcon from "./loader.svg?react"
+import MoonIcon from "./moon.svg?react"
+import SunIcon from "./sun.svg?react"
+import TasksIcon from "./tasks.svg?react"
+import Tasks2Icon from "./tasks-2.svg?react"
+import TrashIcon from "./trash.svg?react"
+
+export {
+  AddIcon,
+  ArrowLeftIcon,
+  CheckIcon,
+  ChevronRightIcon,
+  CloudSunIcon,
+  DetailsIcon,
+  GlassWaterIcon,
+  HomeIcon,
+  LoaderIcon,
+  MoonIcon,
+  SunIcon,
+  Tasks2Icon,
+  TasksIcon,
+  TrashIcon,
+}
