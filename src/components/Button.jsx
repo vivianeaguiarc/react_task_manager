9d99986ab8e6ac6bcd7b02d917bcf14adcdf3196
Viviane <vivianeaguiarc@outlook.com>
2025-12-05 11:54:58 -0300

    feat: add size and secondary variant Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,11 @@
 /* eslint-disable no-unused-vars */
-const Button = ({ children, variant = "primary", ...rest }) => {
+const Button = ({
+  children,
+  variant = "primary",
+  size = "small",
+  className,
+  ...rest
+}) => {
   const getVariantClasses = () => {
     switch (variant) {
       case "primary":
@@ -7,12 +13,22 @@ const Button = ({ children, variant = "primary", ...rest }) => {
 
       case "ghost":
         return "bg-transparent text-[#818181]"
+
+      case "secondary":
+        return "bg-[#eeeeee] text-[#35383e]"
     }
   }
-
+  const getSizeClasses = () => {
+    if (size === "small") {
+      return "py-1 text-xs"
+    }
+    if (size === "large") {
+      return "py-2 text-md"
+    }
+  }
   return (
     <button
-      className={`flex items-center gap-2 rounded-md px-3 py-1 text-xs font-semibold transition hover:opacity-80 ${getVariantClasses()}`}
+      className={`flex items-center justify-center gap-2 rounded-md px-3 ${getSizeClasses()} font-semibold transition hover:opacity-80 ${getVariantClasses()} ${className}`}
       {...rest}
     >
       {children}
