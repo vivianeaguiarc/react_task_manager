c04e15d46f03f0e9f51b7acf10a948bf59a36a0e
Viviane <vivianeaguiarc@outlook.com>
2025-12-08 11:01:41 -0300

    feat: add react router dom and initial TaskDetailsPage

--- src/pages/task-details.jsx
+++ src/pages/task-details.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable no-undef */
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 const TaskDetailsPage = () => {
   const { taskId } = useParams()
+  const [task, setTask] = useState()
 
   useEffect(() => {
     const fetchTask = async () => {
@@ -11,10 +12,16 @@ const TaskDetailsPage = () => {
         method: "GET",
       })
       const data = await response.json()
+      setTask(data)
     }
     fetchTask()
   }, [taskId])
-  return <h1>Task Details</h1>
+  return (
+    <div>
+      <h1>{task?.title}</h1>
+      <p>{task?.description}</p>
+    </div>
+  )
 }
 
 export default TaskDetailsPage
